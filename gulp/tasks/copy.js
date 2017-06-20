9136f7dcf084b64421a4060080c658bfb87d363f
Aleksandr Kirgetov <ak@akwd.ru>
2017-06-20 08:13:27 +0300

    menu added

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -5,7 +5,7 @@ module.exports = function() {
         var source = ['source/images/**/*', 'source/fonts/**/*'];
         var destination = 'build/assets/';
         return $.gulp
-            .src(source, { 'base' : './source/' })
+            .src(source, { 'base' : './source/', since: $.gulp.lastRun('copy') })
             .pipe($.gulp.dest(destination));
     });
 };
